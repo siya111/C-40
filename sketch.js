a506db94cb8eb5b31b0e1800b2f24c4c33f38ab9
siya111 <prachi.subscriptions@gmail.com>
2020-04-16 17:45:35 +0530

    abc

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
-var ball;
+var car;
+var database , position;
 
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    car = createSprite(250,250,10,10);
+    car.shapeColor = "red";
+    var carposition = database.ref('ball/position');
+    carposition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+     database.ref('ball/position').set({
+         'x':position.x + x,
+         'y':position.y + y,
+     })
+}
+
+function readPosition(data){
+    position = data.val();
+    car.x = position.x;
+    car.y = position.y;
+}
+
+function showError(){
+    console.log("error in writing to the database");
 }
